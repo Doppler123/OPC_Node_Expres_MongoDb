e50a0760f62b1f064bca5bc6dd95f723432e82f7
Doppler <thomas.bertin@yahoo.fr>
2022-07-19 22:28:42 +0200

    better development of the server + test of successive middlewares on the express app

--- app.js
+++ app.js
@@ -1,5 +1,28 @@
+
 const express = require('express');
 
 const app = express();
+
+// Cette application Express contient quatre éléments de middleware :
+// 1 middleware = 1 fonction dans une application express qui reçoit la requête et la réponse, qui les gère et qui peut ensuite passer l'éxécution à un prochain middleware 
+
+app.use((req, res, next) => { // le premier enregistre « Requête reçue ! » dans la console et passe l'exécution ;
+  console.log('Requête reçue !');
+  next(); // sans cette ligne, on ne renvoit pas de réponse et la requête ne se termine pas
+});
+
+app.use((req, res, next) => { // le deuxième ajoute un code d'état 201 à la réponse et passe l'exécution ;
+  res.status(201);
+  next();
+});
+
+app.use((req, res, next) => { // le troisième envoie la réponse JSON et passe l'exécution ;
+  res.json({ message: 'Votre requête a bien été reçue !' }); 
+  next();
+});
+
+app.use((req, res, next) => { // le dernier élément de middleware enregistre « Réponse envoyée avec succès ! » dans la console.
+  console.log('Réponse envoyée avec succès !');
+});
 
 module.exports = app;
